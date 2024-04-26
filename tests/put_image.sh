f8914ff3b72d1bd64c4f97a62ae58a6a6d6289fa
vinnix <vinnix@gmail.com>
2024-04-26 18:18:13 +0100

    Image upload tester - using curl sucessefully done
    Using this test with CURL special parameters was possible
    to submit a RESTful PUT request to our application which received
    and stored the file.

--- tests/put_image.sh
+++ tests/put_image.sh
@@ -1,10 +1,18 @@
 #!/bin/env bash
 
-
 file_path="./"
 file_name="chinese-dragon.png"
-host_addr="172.17.0.2:5100"
+host_addr="http://127.0.0.1:5100"
 method="/api/submit"
 
-curl -X PUT -T "${file_path}${file_name}" "${host_addr}${method}"
+RESULT=$( curl -i -X POST -H "Content-Type: multipart/form-data" -F "file=@${file_path}${file_name}" "${host_addr}${method}")
 
+if [[ $RESULT =~ "302" ]];
+then
+    echo "SUCESS!"
+    echo "Upload done!"
+else
+    echo "!!! ERROR !!!"
+    echo "Full output:"
+    echo ${RESULT}
+fi

f9881b581e4e2d73bf45308d6460316636b70b71
vinnix <vinnix@gmail.com>
2024-05-07 09:01:41 +0100

    Uploading file now with templates

--- app/flask/experiments.py
+++ app/flask/experiments.py
@@ -232,23 +232,17 @@ api.add_resource(Car, '/car/<record_id>')
 
 
 @app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
+def upload():
     if request.method == 'POST':
         if 'file1' not in request.files:
-            return 'there is no file1 in form!'
-        file1 = request.files['file1']
-        path = os.path.join(app.config['UPLOAD_FOLDER'], file1.filename)
-        file1.save(path)
-        return path
+            return render_template('upload.html',msg="there is no file in form!")
+        file_data = request.files['file1']
+        json_data=request.form.get("json_data")
+        jd = json.loads(json_data)
 
-        return 'ok'
-        return '''
-        <h1>Upload new File</h1>
-        <form method="post" enctype="multipart/form-data">
-        <input type="file" name="file1">
-        <input type="submit">
-        </form>
-        '''
+        insert_car(jd['carname'],jd['company'],file_data.read())
+        return render_template('upload.html',msg="upload done!")
+    return render_template('upload.html',msg="there is no file in form!")
 
 
 @app.route('/ui/<path:path>')
